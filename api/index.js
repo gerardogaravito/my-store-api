1423b99b69b92fe0e318594fc05d1c85d8198e6a
Gerardo Garavito <garavitogerardo@gmail.com>
2024-03-21 12:47:34 -0600

    fix: cors origin validation

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 const whitelist = ['http://localhost:8080', 'https://garavito.dev']
 const options = {
   origin: (origin, callback) => {
-    if (whitelist.includes(origin || !origin)) {
+    if (whitelist.includes(origin) || !origin) {
       callback(null, true)
     }
     callback(new Error('no permitido'))
